2c354e97102c22fad3f88956e635d490dea5d0d4
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-25 00:07:21 +0200

    Implement methodds for prompt and get input

--- Sudoku_Game.rb
+++ Sudoku_Game.rb
@@ -21,4 +21,33 @@ class SudokuGame
             end
         end
     end
+    
+    def prompt_for_pos
+        puts "Enter a position of tile Ex.`0 2`"
+    end
+
+    def input_pos
+        pos = gets.chomp.split
+        pos.map(&:to_i)
+    end
+
+    def valid_pos?(pos)
+        return false if not pos 
+        return false if pos.length != 2
+        pos.all? { |i| i.between?(0, 9) }
+    end
+
+    def prompt_for_value
+        puts "Enter a new value for the tile Ex.`5`"
+    end
+
+    def input_value
+        val = gets.chomp
+        val.to_i
+    end
+
+    def valid_value?(val)
+        return false if not val
+        val.between?(1, 9)
+    end
 end
