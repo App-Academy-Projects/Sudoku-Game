86000a44102264a5a1c0c067832805326d19c0ca
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-24 19:20:06 +0200

    Methods for each Tile

--- Tile.rb
+++ Tile.rb
@@ -1,3 +1,18 @@
 class Tile
-    
+    attr_accessor :value
+    def initialize(value)
+        @value = value
+        @given = false
+    end
+
+    def to_s
+        self.value.to_s
+    end
+
+    def change_tile(new_value)
+        self.value = new_value if !self.given
+    end
+
+    private
+    attr_accessor :given
 end
