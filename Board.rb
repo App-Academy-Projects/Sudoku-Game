fde2daf8bb05fe3845cd1e8bf4efaf94b5a1fd92
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-24 21:04:22 +0200

    Read Data from file

--- Board.rb
+++ Board.rb
@@ -1,5 +1,13 @@
 class Board
-    def initialize
-        
+    def initialize(grid)
+        @grid = grid
+    end
+
+    def self.from_file(file)
+        file_data = read_line(file).map { |line| line.split("") }
+    end
+
+    def read_line(file)
+        File.read(file).split
     end
 end
